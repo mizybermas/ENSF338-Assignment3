9f217a657cb074f8ae853aa2d0b639082c72b345
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2023-03-14 13:19:56 -0600

    cleaned up unused code/ commented code

--- ex3.2.py
+++ ex3.2.py
@@ -30,73 +30,10 @@ def binary_search(arr, target, initial_midpoint):
     return -1  # Target not found in array
 
 
-def binary_search_interpolation(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-
-        # first iteration uses interpolation search for midpoint
-        if left == 0 and right == len(arr) - 1:
-            mid = (left + right) // 2
-            mid = int(
-                left + (((float(right - left)/(arr[right] - arr[left])) * (target - arr[left]))))
-            # print(f"{target} has midpoint: {mid}")
-        else:
-            mid = (left + right) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return -1
-
-
 data = json.loads(requests.get(dataUrl).text)
 
 searchTasks = json.loads(requests.get(searchTasksUrl).text)
-iwin = 0
-bwin = 0
-reg = []
-inter = []
-# for i in searchTasks:
-#     start_interp = time.time()
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     binary_search_interpolation(data, i)
-#     end_interp = time.time()
-#     inter.append(end_interp-start_interp)
 
-# for i in searchTasks:
-#     start_reg = time.time()
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     binary_search(data, i)
-#     end_reg = time.time()
-#     reg.append(end_reg-start_reg)
-
-
-# for i in range(len(reg)):
-#     if reg[i] > inter[i]:
-#         iwin += 1
-#     else:
-#         bwin += 1
 x = []
 y = []
 
